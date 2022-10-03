525a58bb4b70046cab768f3af18eb421d0a05800
Khushwinder Singh <khushwindersinghdhillon79@gmail.com>
2022-10-03 11:39:21 -0400

    added table information with ids in class

--- src/main/java/com/example/f22comp1011s1w1/SongTableViewController.java
+++ src/main/java/com/example/f22comp1011s1w1/SongTableViewController.java
@@ -2,10 +2,30 @@ package com.example.f22comp1011s1w1;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
 import java.io.IOException;
 
 public class SongTableViewController {
+
+    @FXML
+    private TableColumn<Song, String> artist;
+
+    @FXML
+    private TableColumn<Song, String> genre;
+
+    @FXML
+    private TableColumn<Song, Integer> length;
+
+    @FXML
+    private TableColumn<Song, String> name;
+
+    @FXML
+    private TableColumn<Song, Integer> songID;
+
+    @FXML
+    private TableView<Song> tableView;
 
     @FXML
     void createSongScene(ActionEvent event) throws IOException {
